bf2d040d41a9d173831d17ed913af59804b91bfb
Max Fuller <max.fuller01@btinternet.com>
2018-06-01 17:40:01 +0100

    Update simulation code to correctly model the robot

--- matlab/fw_DH.m
+++ matlab/fw_DH.m
@@ -10,26 +10,31 @@ s_5 = 30;
 
 % Create and configure RobotArm object and set initial state
 arm = RobotArm();
-arm.addJoint(0, s_b,   0,   0,   0);
-arm.addJoint(0, s_0,   0,   0,  90);
-arm.addJoint(0,   0,   0, s_1,   0);
-arm.addJoint(0,   0,   0, s_2,   0);
-arm.addJoint(0, s_3,   0,   0, 270);
-arm.addJoint(0,   0,   0,   0,  90);
-arm.addJoint(0, s_5,   0,   0,   0);
-positions = arm.update([0; 0; 0; 0; 0; 0; 0;]);
+arm.addJoint(0,     s_b,   0,     0,  0); % b
+arm.addJoint(0,     s_0,   0,     0, 90); % 0
+arm.addJoint(0,       0,   0,   s_1,  0); % 1
+arm.addJoint(0,       0,   180,   0, 90); % 2
+arm.addJoint(0, s_2+s_3,   180,   0, 90); % 3
+arm.addJoint(0,       0,   180,   0, 90); % 4
+arm.addJoint(0, s_4+s_5,   180,   0,  0); % 5
+positions = arm.update(zeros(1,7));
 
 % Create GUI object and show initial arm state
+close all;
 gui = GUI();
+gui.setResetCallback(@arm.reset);
 gui.show(positions);
 
 
+%return;
 iterations = 100;
 
 for n = 1:iterations
-    positions = arm.update([0; (360*(n/iterations)); 0; 0; 0; 0; 0;]);
+    angle = (360*(n/iterations)*1.3);
+    disp(angle);
+    positions = arm.update([0; angle; 0; 0; 0; 0; 0;]);
     gui.update(positions);
     
     pause(0.05);
-    disp(n);
+    %disp(n);
 end
