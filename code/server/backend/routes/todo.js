2063759303423fb430930610d721f6da6e0245fb
Max Fuller <max.fuller01@btinternet.com>
2018-06-02 16:10:43 +0100

    Add todo create and update routes

--- code/server/backend/routes/todo.js
+++ code/server/backend/routes/todo.js
@@ -13,6 +13,10 @@ router.get("/", (req, res) => {
 
     Todo
         .find({})
+        .sort([
+            ["created", 1],
+            ["completed", 1]
+        ])
         .exec((err, todos) => {
             if (err) {
                 return res.status(404).send("Unable to retrieve todos");
@@ -21,6 +25,46 @@ router.get("/", (req, res) => {
             // Return todos
             res.json(todos);
         });
+});
+
+
+// Create
+router.post("/", (req, res) => {
+    const todo = { 
+        text: req.body.text,
+    };
+    console.log(req.body);
+    console.log(todo);
+
+    Todo.create(todo, (err, createdTodo) => {
+        if (err) {
+            return res.status(404).send("Unable to save todo data");
+        }
+
+        res.json(createdTodo);
+    });
+});
+
+
+// Update
+router.put("/:id", (req, rese) => {
+    Todo.findById(req.params.id, (err, existingTodo) => {
+        if (err) {
+            return res.status(404).send("Unable to retreive todo data");
+        }
+
+        const todo = req.body;
+
+        existingTodo.text = todo.text;
+        existingTodo.completed = todo.completed;
+        existingTodo.save((err, updatedTodo) => {
+            if (err) {
+                return res.status(404).send("Unable to update todo data");
+            }
+
+            res.json(updatedTodo);
+        });
+    });
 });
 
 
